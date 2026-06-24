882d5c18ea661be013d514a72ff6f1bcd8dc09b5
Hubert <hubert@forschfrontiers.com>
2026-06-24 02:19:11 +0000

    fix: import api modules so @frappe.whitelist() decorators register
    cockpit.py, app_ops.py, ingest.py, parity.py were not being loaded
    because api/__init__.py was empty. Frappe needs the module imported
    to register whitelisted methods.

---
+++ forsch_frontiers/api/__init__.py
@@ -0,0 +1,5 @@
+# Import API modules so @frappe.whitelist() decorators register.
+from forsch_frontiers.api import cockpit  # noqa: F401
+from forsch_frontiers.api import app_ops  # noqa: F401
+from forsch_frontiers.api import ingest   # noqa: F401
+from forsch_frontiers.api import parity   # noqa: F401
